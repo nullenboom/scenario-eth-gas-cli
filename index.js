26a2a8428bd16ff5efe34f6b725301bc8c65ec8e
Niklas Ullenboom <niklas.ullenboom@paluno.uni-due.de>
2019-11-30 13:54:15 +0100

    Adds first files and dependecies

---
+++ index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+
+const files = require('./lib/files');
+
+clear();
+
+console.log(
+  chalk.yellow(
+    figlet.textSync('Scenario CLI', { horizontalLayout: 'full' })
+  )
+);
+
+/*const run = async () => {
+  try {
+    // Retrieve & Set Authentication Token
+    const token = await getGithubToken();
+    github.githubAuth(token);
+
+    // Create remote repository
+    const url = await repo.createRemoteRepo();
+
+    // Create .gitignore file
+    await repo.createGitignore();
+
+    // Set up local repository and push to remote
+    await repo.setupRepo(url);
+    console.log(chalk.green('All done!'));
+  } catch(err) {
+      if (err) {
+        switch (err.status) {
+          case 401:
+            console.log(chalk.red('Couldn\'t log you in. Please provide correct credentials/token.'));
+            break;
+          case 422:
+            console.log(chalk.red('There already exists a remote repository with the same name'));
+            break;
+          default:
+            console.log(err);
+        }
+      }
+  }
+};
+
+run();*/
